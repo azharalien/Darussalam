230e7b09769f1df1fc68e7d74796d90fa289d015
JoeLewis <bjoe_lewis@yahoo.com>
2012-02-05 13:11:21 +0530

    fixed margin bugs

--- proj.php
+++ proj.php
@@ -3,7 +3,7 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 
-<title>IslamicBookspot.com</title>
+<title></title>
 <link href="../WebPro/general.css" rel="stylesheet" type="text/css">
 <link href="../WebPro/comments.css" rel="stylesheet" type="text/css">
 <link href="../WebPro/dragdrop.css" rel="stylesheet" type="text/css">
